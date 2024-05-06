2bafef2df1d9f9819b513afaac41e245db776c49
Panagiotis <panostsomp0211@gmail.com>
2024-05-07 00:22:05 +0300

    build

--- build_compiler.sh
+++ build_compiler.sh
@@ -6,18 +6,18 @@ Copy code
 set -e
 
 # Generate the lexer and parser source files
-flex -o src/ts_lexer.c src/ts_lexer.l
-bison -d -o src/ts_parser.c src/ts_parser.y
+flex -o build/ts_lexer.c src/ts_lexer.l
+bison -d -o build/ts_parser.c src/ts_parser.y
 
 # Compile the lexer, parser, and other source files
-gcc -Wall -Isrc -c src/ts_parser.c -o src/ts_parser.o
-gcc -Wall -Isrc -c src/ts_lexer.c -o src/ts_lexer.o
-gcc -Wall -Isrc -c src/semantic_analysis.c -o src/semantic_analysis.o
-gcc -Wall -Isrc -c src/code_generation.c -o src/code_generation.o
-gcc -Wall -Isrc -c src/main.c -o src/main.o
+gcc -Wall -Isrc -c build/ts_parser.c -o build/ts_parser.o
+gcc -Wall -Isrc -c build/ts_lexer.c -o build/ts_lexer.o
+gcc -Wall -Isrc -c src/semantic_analysis.c -o build/semantic_analysis.o
+gcc -Wall -Isrc -c src/code_generation.c -o build/code_generation.o
+gcc -Wall -Isrc -c src/main.c -o build/main.o
 
 # Link all object files into the executable
-gcc src/ts_parser.o src/ts_lexer.o src/semantic_analysis.o src/code_generation.o src/main.o -o bin/tsompCC -lfl
+gcc build/ts_parser.o build/ts_lexer.o build/semantic_analysis.o build/code_generation.o build/main.o -o build/tsompCC -lfl
 
 
 echo "Build completed successfully."
